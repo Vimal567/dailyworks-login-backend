223610024fa7733e21655ce75d9858623a1a77a7
Vimal567 <vimalrock82@gmail.com>
2023-02-18 16:34:25 +0530

    cors updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const mongo_url = "mongodb+srv://vimal:vimal@dailyworks-login-backen.drex5go.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 8082;
+const cors = require("cors");
 const routes = require("./routes/user.route");
 
 mongoose.set("strictQuery", false);
@@ -11,6 +12,8 @@ mongoose.connect(mongo_url).then(() => {
     console.log("connected to mongodb");
 
     app.use(express.json());
+    app.use(cors());
+    app.options("*", cors());
     app.use(routes);
     
     app.listen(PORT, () => {
